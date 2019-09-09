be611774f58e53ffe7777b9bc752e6ce83d0b102
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-09 11:02:53 +0200

    first commit

---
+++ src/main/java/Cirpher.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.List;
+public class Cirpher {
+    public ArrayList<Object> runCirpher(int shiftKey){
+        ArrayList<Object> result = new ArrayList<Object>();
+        result.add(3);
+        return result;
+    }
+}
